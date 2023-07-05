0612207ae10c0ef21d01ec577197d91816b093f4
naveendevarala98 <projectcode98@gmail.com>
2023-07-05 19:18:08 +0530

    Segmentation fault

---
+++ Recusrion/src/RecursionProblem/TestRecursionSegmentation.java
@@ -0,0 +1,16 @@
+package RecursionProblem;
+
+public class TestRecursionSegmentation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		seg();
+
+	}
+	
+	static void seg() {
+		System.out.println("1");
+		seg();
+	}
+
+}
